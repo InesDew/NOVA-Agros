544e8a0fef18a5389b449a1118e2a3d5aff0e0fd
Ines Dewever <ines@dewever.be>
2023-02-21 13:02:57 +0100

    class setup

---
+++ class/group_16.py
@@ -0,0 +1,11 @@
+## This is the python file for our class that will have several methods
+
+# Innit method
+self.data = None
+
+## Method 1: 
+# Download the data file into a /downloads directory 
+#But only do this once
+# Read the dataset into a pandas dataframe which is an attribute of your class (self.data)
+function(self)
+self.data = dataframe
