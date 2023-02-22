ec2133e603f82d7c3b379bcbef40c12590e5ab37
Ines Dewever <ines@dewever.be>
2023-02-22 21:54:43 +0100

    added method 1 to bracnh method3 to make it work

--- class/group_16.py
+++ class/group_16.py
@@ -1,5 +1,3 @@
-<<<<<<< Updated upstream
-=======
 import pandas as pd
 import requests
 import os
@@ -8,33 +6,47 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from string import ascii_letters
 
->>>>>>> Stashed changes
 ## This is the python file for our class that will have several methods
-class MyClass:
+class Agros:
 
 ## Innit method:
 ################
-def __init__(self):
-    self.data = None
+    def __init__(self):
+        self.data = None
 
 ## Method 1:
 ############
- 
-# Download the data file into a /downloads directory
- 
-#But only do this once
 
-# Read the dataset into a pandas dataframe which is an attribute of your class (self.data)
-def method_1(self):
-    dataframe = read(dataset)
-    self.data = dataframe
+    def data_setup(self):
+        #1. We get the path of our new directory downloads we will be making by adding the path of this .py file and "/downloads"
+        absolute_path = os.path.dirname(__file__)
+        relative_path = "/downloads"
+        full_path = absolute_path + relative_path 
+
+        #2. But we're only allowed to make this new directory once. So we check before making if /downloads already exists. 
+        # If it does not exist, we make this downloads directory using the function os.mkdir()
+        if not os.path.exists(full_path):
+            os.mkdir(full_path)
+
+        #3. This is the url where our dataset is located online
+        url = "https://raw.githubusercontent.com/owid/owid-datasets/master/datasets/Agricultural%20total%20factor%20productivity%20(USDA)/Agricultural%20total%20factor%20productivity%20(USDA).csv"
+        #4. We download the info from this url using the requests library and put the response.text in a csv file dataset.csv that we create in the downloads dir
+        response = requests.get(url)
+        file_path = os.path.join(full_path,'dataset.csv')
+        with open(file_path, "w") as f:
+            f.write(response.text)
+
+        #5. Read the dataset.csv file into a pandas dataframe and make it an attribute of our class (self.data)
+        #6. Only take into account data after 1970 (1970 included)
+        dataframe = pd.read_csv(file_path)
+        self.data = dataframe
+
 
 
 ## Method 2:
 ############
 
 # Show a list of all available countries in the dataset
-output = list(countries)
 
 
 ## Method 3:
@@ -43,12 +55,6 @@ output = list(countries)
 #Plot an area chart of consumption (columns biofuel_consumption, coal_consumption, fossil_fuel_consumption, gas_consumption, 
 # hydro_consumption, low_carbon_consumption, nuclear_consumption, oil_consumption, other_renewable_consumption, primary_energy_consumption, 
 # renewables_consumption, solar_consumption, wind_consumption)
-
-<<<<<<< Updated upstream
-#needs a country argument and a normalize argument
-
-#return a ValueError when the chosen country does not exist
-=======
     def correlate__quantities(self):
         # Correlation between different variables
         corr = self.data[["output_quantity","crop_output_quantity","animal_output_quantity","fish_output_quantity","ag_land_quantity","labor_quantity",
@@ -62,7 +68,6 @@ output = list(countries)
         cmap = sns.diverging_palette(230, 20, as_cmap=True)
         # Draw the heatmap with the mask and correct aspect ratio
         sns.heatmap(corr, mask=mask, cmap=cmap, vmax=.3, center=0, square=True, linewidths=.5, cbar_kws={"shrink": .5})
->>>>>>> Stashed changes
 
 
 ## Method 4:
@@ -91,11 +96,6 @@ output = list(countries)
 #input: a year (in integer format)
 #If it is not an integer, the method should raise a TypeError
 
-<<<<<<< Updated upstream
-#scatter plot where x is gdp, y is total energy consumption, and the area of each dot is population.
-def gapminder(year:int):
-    scatterplot
-=======
 # Scatter plot where x is fertilizer_quantity, y is output_quantity, and the area of each dot should be a third relevant variable you find with exploration of the data.
 
 
@@ -104,4 +104,3 @@ def gapminder(year:int):
 object_1 = Agros()
 object_1.data_setup()
 object_1.correlate__quantities()
->>>>>>> Stashed changes
