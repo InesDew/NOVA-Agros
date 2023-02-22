20cc84ef109e1e8568d13bedccb8db731c49a55d
Ines Dewever <ines@dewever.be>
2023-02-22 21:26:25 +0100

    added method 3 first draft

--- class/group_16.py
+++ class/group_16.py
@@ -1,3 +1,14 @@
+<<<<<<< Updated upstream
+=======
+import pandas as pd
+import requests
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from string import ascii_letters
+
+>>>>>>> Stashed changes
 ## This is the python file for our class that will have several methods
 class MyClass:
 
@@ -33,9 +44,25 @@ output = list(countries)
 # hydro_consumption, low_carbon_consumption, nuclear_consumption, oil_consumption, other_renewable_consumption, primary_energy_consumption, 
 # renewables_consumption, solar_consumption, wind_consumption)
 
+<<<<<<< Updated upstream
 #needs a country argument and a normalize argument
 
 #return a ValueError when the chosen country does not exist
+=======
+    def correlate__quantities(self):
+        # Correlation between different variables
+        corr = self.data[["output_quantity","crop_output_quantity","animal_output_quantity","fish_output_quantity","ag_land_quantity","labor_quantity",
+                         "capital_quantity","machinery_quantity","livestock_quantity","fertilizer_quantity","animal_feed_quantity","cropland_quantity",
+                         "pasture_quantity","irrigation_quantity"]].corr()
+        # Generate a mask for the upper triangle
+        mask = np.triu(np.ones_like(corr, dtype=bool))
+        # Set up the matplotlib figure
+        f, ax = plt.subplots(figsize=(11, 9))
+        # Generate a custom diverging colormap
+        cmap = sns.diverging_palette(230, 20, as_cmap=True)
+        # Draw the heatmap with the mask and correct aspect ratio
+        sns.heatmap(corr, mask=mask, cmap=cmap, vmax=.3, center=0, square=True, linewidths=.5, cbar_kws={"shrink": .5})
+>>>>>>> Stashed changes
 
 
 ## Method 4:
@@ -64,6 +91,17 @@ output = list(countries)
 #input: a year (in integer format)
 #If it is not an integer, the method should raise a TypeError
 
+<<<<<<< Updated upstream
 #scatter plot where x is gdp, y is total energy consumption, and the area of each dot is population.
 def gapminder(year:int):
     scatterplot
+=======
+# Scatter plot where x is fertilizer_quantity, y is output_quantity, and the area of each dot should be a third relevant variable you find with exploration of the data.
+
+
+# Calling of the methods
+########################
+object_1 = Agros()
+object_1.data_setup()
+object_1.correlate__quantities()
+>>>>>>> Stashed changes
